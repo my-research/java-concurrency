064e1649b3ada0c3f46edc761048870fe97588ac
dhslrl321 <dhslrl321@gmail.com>
2022-11-19 10:16:10 +0900

    commit

---
+++ src/main/java/com/example/javaconcurrency/JavaConcurrencyApplication.java
@@ -0,0 +1,14 @@
+package com.example.javaconcurrency;
+
+import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaConcurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaConcurrencyApplication.class, args);
+    }
+
+}
